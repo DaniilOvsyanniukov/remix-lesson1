5c3c9431d92bab60dd87020241869a77ccafc026
Даниил Овсянников <ovsuanikov@gmail.com>
2022-06-03 14:03:43 +0300

    upgrade  proxy to becon proxy

--- test/HouseRegistryExtV2.ts
+++ test/HouseRegistryExtV2.ts
@@ -19,15 +19,22 @@ describe('HouseRegistryExtV2', function () {
     const DaiToken = await ethers.getContractFactory('DaiToken', acc3);
     daiToken = await DaiToken.deploy();
     daiAddress = daiToken.address;
-    houseRegistryExt = await upgrades.deployProxy(HouseRegistryExt, [daiAddress], {
+    const beacon = await upgrades.deployBeacon(HouseRegistryExt);
+    houseRegistryExt = await upgrades.deployBeaconProxy(beacon, HouseRegistryExt, [daiAddress], {
       initializer: 'init',
     });
-    await houseRegistryExt.deployed();
+
+    // houseRegistryExt = await upgrades.deployProxy(HouseRegistryExt, [daiAddress], {
+    //   initializer: 'init',
+    // });
+    // await houseRegistryExt.deployed();
     await daiToken.deployed();
     const func = await houseRegistryExt.connect(acc2).listHouseSimple(103, 103, 103, 'asd');
     const result = await func.wait();
     houseId = result.events[0].args[0];
-    houseRegistryExtV2 = await upgrades.upgradeProxy(houseRegistryExt.address, HouseRegistryExtV2);
+    await upgrades.upgradeBeacon(beacon, HouseRegistryExtV2);
+    houseRegistryExtV2 = HouseRegistryExtV2.attach(houseRegistryExt.address);
+    // houseRegistryExtV2 = await upgrades.upgradeProxy(houseRegistryExt.address, HouseRegistryExtV2);
   });
 
   it('should be deployed', async function () {
@@ -70,19 +77,19 @@ describe('HouseRegistryExtV2', function () {
     await expect(result.events[0].args[0]).to.equal('Transactions succesful');
   });
 
-  it('should return expensive houseId', async function () {
-    const expensiveHouse = await houseRegistryExtV2
-      .connect(acc3)
-      .listHouseSimple(200, 200, 200, 'asd');
-    await houseRegistryExtV2.connect(acc1).listHouseSimple(150, 150, 150, 'asd');
+  it('should update contract and return houseId from first contract', async function () {
+    await houseRegistryExtV2.connect(acc3).listHouseSimple(100, 100, 100, 'asd');
+    await houseRegistryExtV2.connect(acc1).listHouseSimple(101, 101, 101, 'asd');
+    const func = await houseRegistryExtV2.getExpensiveHouseIds();
+    await expect(func).to.equal(houseId);
+  });
+
+   it('should return expensive houseId', async function () {
+    await houseRegistryExtV2.connect(acc3).listHouseSimple(100, 100, 100, 'asd');
+    const expensiveHouse = await houseRegistryExtV2.connect(acc1).listHouseSimple(200, 200, 200, 'asd');
     const result = await expensiveHouse.wait();
     const expensuveHouseId = result.events[0].args[0];
     const func = await houseRegistryExtV2.getExpensiveHouseIds();
     await expect(func).to.equal(expensuveHouseId);
-  });
-
-  it('should return cheepest houseId created in V1 Contract', async function () {
-    const func = await houseRegistryExtV2.getCheapHouseIds(103);
-    await expect(func[0]).to.deep.equal(houseId);
   });
 });
