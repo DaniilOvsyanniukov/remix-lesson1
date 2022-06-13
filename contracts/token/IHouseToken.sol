3e4eafa0417593032891c50e184c08a3249240b4
Даниил Овсянников <ovsuanikov@gmail.com>
2022-06-13 12:06:13 +0300

    size of contracts

--- contracts/token/IHouseToken.sol
+++ contracts/token/IHouseToken.sol
@@ -3,10 +3,20 @@
 
 pragma solidity ^0.8.2;
 
+import './HouseToken.sol';
+
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 
 interface IHouseToken is IERC721 {
-    function changeBuyerAddress(address buyerAddress) external;
+    function changeBuyerAddress(address _buyerAddress) external;
 
     function delistHouse() external;
+
+    function sellerAddress() external view returns (address);
+
+    function price() external view returns (uint256);
+
+    function id() external view returns (uint256);
+
+    function buyerAddress() external view returns (address);
 }
